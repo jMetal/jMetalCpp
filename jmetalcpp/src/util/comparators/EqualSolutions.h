87d28718213ac652c215098ced30ecddc77d6d18
estebanlc <esteban@lcc.uma.es>
2011-12-13 13:41:32 +0100

    Added comparator CrowdingDistanceComparator by estebanlc Added comparator EqualSolutions by estebanlc

---
+++ jmetalcpp/src/util/comparators/EqualSolutions.h
@@ -0,0 +1,26 @@
+/**
+ * @file EqualSolutions.h
+ * @author Esteban López
+ * @date 13 December 2011
+**/
+
+#ifndef __EQUAL_SOLUTIONS__
+#define __EQUAL_SOLUTIONS__
+
+#include <Solution.h>
+#include <Comparator.h>
+#include <ObjectiveComparator.h>
+
+/**
+ * This class implements a <code>Comparator</code> (a method for comparing
+ * <code>Solution</code> objects) based whether all the objective values are
+ * equal or not. A dominance test is applied to decide about what solution
+ * is the best.
+ */
+class EqualSolutions : public Comparator {
+
+public:
+   int compare(Solution *solution1, Solution *solution2);
+};
+
+#endif
