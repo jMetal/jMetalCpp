4f449d0ff0a2ce70549aaf5985fb40172cdc8cd4
ajnebro <antelverde@gmail.com>
2012-02-02 18:06:09 +0100

    Added problem DTLZ5

---
+++ jmetalcpp/src/problems/DTLZ5.cpp
@@ -0,0 +1,89 @@
+/*
+ * DTLZ5.cpp
+ *
+ *  Created on: 09/12/2011
+ *      Author: antonio
+ */
+
+#include <DTLZ5.h>
+
+DTLZ5::DTLZ5(string solutionType, int numberOfVariables, int numberOfObjectives) {
+	numberOfVariables_   = numberOfVariables;
+	numberOfObjectives_  = numberOfObjectives;
+	numberOfConstraints_ = 0;
+	problemName_ 				 = "DTLZ5";
+
+	lowerLimit_ = new double[numberOfVariables_];//(double *)malloc(sizeof(double)*numberOfVariables);
+	if (lowerLimit_ == NULL) {
+		cout << "Impossible to reserve memory for storing the variable lower limits" << endl;
+		exit(-1);
+	}
+
+	upperLimit_ = new double[numberOfVariables_];//(double *)malloc(sizeof(double)*numberOfVariables);
+	if (upperLimit_ == NULL) {
+		cout << "Impossible to reserve memory for storing the variable lower limits" << endl;
+		exit(-1);
+	}
+
+	for (int i = 0; i < numberOfVariables_; i++) {
+		lowerLimit_[i] = 0.0;
+		upperLimit_[i] = 1.0;
+	}
+
+	if (solutionType.compare("BinaryReal") == 0)
+		solutionType_ = new BinaryRealSolutionType(this) ;
+	else if (solutionType.compare("Real") == 0) {
+		solutionType_ = new RealSolutionType(this) ;
+		cout << "Tipo seleccionado Real" << endl;
+	}
+	else if (solutionType.compare("ArrayReal") == 0)
+		solutionType_ = new ArrayRealSolutionType(this) ;
+	else {
+		cout << "Error: solution type " << solutionType << " invalid" << endl;
+		exit(-1) ;
+	}
+
+	fx_ = new double[numberOfObjectives_] ;
+  x_ = new double[numberOfVariables_];
+}
+
+DTLZ5::~DTLZ5() {
+  delete [] lowerLimit_ ;
+  delete [] upperLimit_ ;
+  delete solutionType_ ;
+}
+
+/**
+ * Evaluates a solution
+ * @param solution The solution to evaluate
+ */
+void DTLZ5::evaluate(Solution *solution) {
+	XReal * vars = new XReal(solution);
+
+  int k = numberOfVariables_ - numberOfObjectives_ + 1;
+  double alpha = 100.0;
+
+  for (int i = 0; i < numberOfVariables_; i++)
+    x_[i] = vars->getValue(i);
+
+  double g = 0.0;
+  for (int i = numberOfVariables_ - k; i < numberOfVariables_; i++)
+    g += (x_[i] - 0.5)*(x_[i] - 0.5);
+
+  for (int i = 0; i < numberOfObjectives_; i++)
+    fx_[i] = 1.0 + g;
+
+  for (int i = 0; i < numberOfObjectives_; i++) {
+    for (int j = 0; j < numberOfObjectives_ - (i + 1); j++)
+      fx_[i] *= cos(pow(x_[j],alpha)*(M_PI/2.0));
+      if (i != 0){
+        int aux = numberOfObjectives_ - (i + 1);
+        fx_[i] *= sin(pow(x_[aux],alpha)*(M_PI/2.0));
+      } //if
+  } // for
+
+  for (int i = 0; i < numberOfObjectives_; i++)
+    solution->setObjective(i, fx_[i]);
+
+	delete vars ;
+} // evaluate
---
+++ jmetalcpp/src/problems/DTLZ5.h
@@ -0,0 +1,30 @@
+/*
+ * DTLZ4.h
+ *
+ *  Created on: 09/12/2011
+ *      Author: antonio
+ */
+
+#ifndef __DTLZ5_H__
+#define __DTLZ5_H__
+
+#include <Problem.h>
+#include <math.h>
+#include <BinaryRealSolutionType.h>
+#include <RealSolutionType.h>
+#include <ArrayRealSolutionType.h>
+#include <XReal.h>
+#include <Solution.h>
+
+class DTLZ5 : public Problem {
+public:
+	DTLZ5(string solutionType, int numberOfVariables = 12, int numberOfObjectives = 3);
+	void evaluate(Solution *solution);
+
+	virtual ~DTLZ5();
+private:
+	double * fx_ ;
+  double * x_  ;
+};
+
+#endif /* __DTLZ5_H__ */
