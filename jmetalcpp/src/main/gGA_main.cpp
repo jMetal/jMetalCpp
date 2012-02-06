f5ce714f08cfe97c84396ac76d89d536a083713c
estebanlc <esteban@lcc.uma.es>
2012-02-06 16:52:18 +0100

    Fixed gGA again

--- jmetalcpp/src/main/gGA_main.cpp
+++ jmetalcpp/src/main/gGA_main.cpp
@@ -7,12 +7,11 @@
 #include <Problem.h>
 #include <Algorithm.h>
 #include <Solution.h>
-#include <RealSolutionType.h>
 #include <SBXCrossover.h>
 #include <PolynomialMutation.h>
 #include <BinaryTournament2.h>
+#include <ProblemFactory.h>
 #include <iostream>
-#include <Sphere.h>
 #include <gGA.h>
 #include <time.h>
 
@@ -26,67 +25,49 @@ int main(int argc, char ** argv) {
   Operator  * mutation  ; // Mutation operator
   Operator  * selection ; // Selection operator
 
-	problem = new Sphere("Real");
-	//problem = new DTLZ1("Real", 7, 2);
-	//problem = new DTLZ3("Real", 12, 2);
-	//problem = new DTLZ4("Real", 12, 2);
-
-	//problem   = new Schaffer("Real");
-	//problem   = new ZDT1("Real", 30);
-
-	cout << "El numero de objetivos es " << problem->getNumberOfObjectives() << endl;
-	cout << "Problema: " << problem->getName() << endl;
+  if (argc>=2) {
+    problem = ProblemFactory::getProblem(argc, argv);
+  } else {
+    // char * defaultProblem;
+    // strcpy(defaultProblem, "Sphere");
+    cout << "No problem selected." << endl;
+    cout << "Default problem will be used: Sphere" << endl;
+    problem = ProblemFactory::getProblem("Sphere");
+  }
 
 	algorithm = new gGA(problem);
 
-	cout << "Algoritmo gGA inicializado." << endl;
+	cout << "Generational genetic algorithm initialized." << endl;
 
 	// Algorithm parameters
-	int populationSizeValue = 100;
-	int *populationSizePtr = &populationSizeValue;
-	int maxEvaluationsValue = 250000;
-	int *maxEvaluationsPtr = &maxEvaluationsValue;
-	algorithm->setInputParameter("populationSize",populationSizePtr);
-	algorithm->setInputParameter("maxEvaluations",maxEvaluationsPtr);
+  int populationSizeValue = 100;
+  int maxEvaluationsValue = 250000;
+  algorithm->setInputParameter("populationSize",&populationSizeValue);
+  algorithm->setInputParameter("maxEvaluations",&maxEvaluationsValue);
 
 	// Mutation and Crossover for Real codification
-	map<string, void *> parameters;
-	double crossoverProbability = 0.9;
-	double distributionIndexValue1 = 20.0;
-	double *distributionIndexPtr1 = &distributionIndexValue1;
-	parameters["probability"] =  &crossoverProbability ;
-	parameters["distributionIndex"] = distributionIndexPtr1 ;
-	crossover = new SBXCrossover(parameters);
+  map<string, void *> parameters;
+  double crossoverProbability = 0.9;
+  double distributionIndexValue1 = 20.0;
+  parameters["probability"] =  &crossoverProbability ;
+  parameters["distributionIndex"] = &distributionIndexValue1 ;
+  crossover = new SBXCrossover(parameters);
 
 	parameters.clear();
-	double mutationProbability = 1.0/problem->getNumberOfVariables();
-	double distributionIndexValue2 = 20.0;
-	double *distributionIndexPtr2 = &distributionIndexValue2;
-	parameters["probability"] = &mutationProbability;
-	parameters["distributionIndex"] = distributionIndexPtr2 ;
-	mutation = new PolynomialMutation(parameters);
+  double mutationProbability = 1.0/problem->getNumberOfVariables();
+  double distributionIndexValue2 = 20.0;
+  parameters["probability"] = &mutationProbability;
+  parameters["distributionIndex"] = &distributionIndexValue2 ;
+  mutation = new PolynomialMutation(parameters);
 
 	// Selection Operator
 	parameters.clear();
 	selection = new BinaryTournament2(parameters) ;
 
-	cout << "Adding operators..." << endl;
-
 	// Add the operators to the algorithm
 	algorithm->addOperator("crossover",crossover);
 	algorithm->addOperator("mutation",mutation);
 	algorithm->addOperator("selection",selection);
-
-	// Checking parameters
-	cout << "SBXCrossover:" << endl;
-	cout << *(double *) algorithm->getOperator("crossover")->getParameter("probability") << endl;
-	cout << *(double *) algorithm->getOperator("crossover")->getParameter("distributionIndex") << endl;
-	cout << "PolynomialMutation:" << endl;
-	cout << *(double *) algorithm->getOperator("mutation")->getParameter("probability") << endl;
-	cout << *(double *) algorithm->getOperator("mutation")->getParameter("distributionIndex") << endl;
-	cout << "BinaryTournament:" << endl;
-	//cout << algorithm->getOperator("selection")->getParameter("probability") << endl;
-	//cout << algorithm->getOperator("selection")->getParameter("distributionIndex") << endl;
 
 	// Add the indicator object to the algorithm
 	//algorithm->setInputParameter("indicators", indicators) ;
@@ -99,8 +80,15 @@ int main(int argc, char ** argv) {
 
 	// Result messages
 	cout << "Total execution time: " << secs << "s" << endl;
-	cout << "Variables values have been writen to file VAR" << endl;
+	cout << "Variables values have been written to file VAR" << endl;
 	population->printVariablesToFile("VAR");
-	cout << "Objectives values have been writen to file FUN" << endl;
+	cout << "Objectives values have been written to file FUN" << endl;
 	population->printObjectivesToFile("FUN");
+
+  delete crossover;
+  delete mutation;
+  delete selection;
+  delete population;
+  delete algorithm;
+
 } // main
--- jmetalcpp/src/metaheuristics/gGA/gGA.cpp
+++ jmetalcpp/src/metaheuristics/gGA/gGA.cpp
@@ -26,141 +26,153 @@ gGA::gGA(Problem *problem) : Algorithm(problem) {
  */
 SolutionSet * gGA::execute() {
 
-	int populationSize;
-	int maxEvaluations;
-	int evaluations;
+  int populationSize;
+  int maxEvaluations;
+  int evaluations;
 
-	SolutionSet * population;
-	SolutionSet * offspringPopulation;
+  SolutionSet * population;
+  SolutionSet * offspringPopulation;
 
-	Operator * mutationOperator;
-	Operator * crossoverOperator;
-	Operator * selectionOperator;
+  Operator * mutationOperator;
+  Operator * crossoverOperator;
+  Operator * selectionOperator;
 
-	Comparator * comparator = new ObjectiveComparator(0) ;
+  Comparator * comparator = new ObjectiveComparator(0) ;
 
-	//Read the parameters
-	populationSize = *(int *) getInputParameter("populationSize");
-	maxEvaluations = *(int *) getInputParameter("maxEvaluations");
-	// TODO: indicators = (QualityIndicator) getInputParameter("indicators");
-	cout << "populationSize = " << populationSize << endl;
-	cout << "maxEvaluations = " << maxEvaluations << endl;
+  //Read the parameters
+  populationSize = *(int *) getInputParameter("populationSize");
+  maxEvaluations = *(int *) getInputParameter("maxEvaluations");
+  // TODO: indicators = (QualityIndicator) getInputParameter("indicators");
+//  cout << "populationSize = " << populationSize << endl;
+//  cout << "maxEvaluations = " << maxEvaluations << endl;
 
-	//Initialize the variables
-	population = new SolutionSet(populationSize);
-	evaluations = 0;
+  //Initialize the variables
+  population = new SolutionSet(populationSize);
+  evaluations = 0;
 
-	cout << "Poblacion inicializada con maxsize = " << population->getMaxSize() << endl;
-	cout << "Poblacion inicializada con size = " << population->size() << endl;
-	cout << "Problema: " << problem_->getName() << endl;
+//  cout << "Poblacion inicializada con maxsize = " << population->getMaxSize() << endl;
+//  cout << "Poblacion inicializada con size = " << population->size() << endl;
+//  cout << "Problema: " << problem_->getName() << endl;
 
+  //Read the operators
+  mutationOperator = operators_["mutation"];
+  crossoverOperator = operators_["crossover"];
+  selectionOperator = operators_["selection"];
 
-	//Read the operators
-	mutationOperator = operators_["mutation"];
-	crossoverOperator = operators_["crossover"];
-	selectionOperator = operators_["selection"];
+//  cout << "Comienza la inicializacion de la poblacion con size " << populationSize << endl;
 
-	cout << "Comienza la inicializacion de la poblacion con size " << populationSize << endl;
+  // Create the initial solutionSet
+  Solution * newSolution;
+  for (int i = 0; i < populationSize; i++) {
+    newSolution = new Solution(problem_);
+    problem_->evaluate(newSolution);
+    problem_->evaluateConstraints(newSolution);
+    evaluations++;
+    population->add(newSolution);
+  } //for
 
-	// Create the initial solutionSet
-	Solution * newSolution;
-	for (int i = 0; i < populationSize; i++) {
-		newSolution = new Solution(problem_);
-		problem_->evaluate(newSolution);
-		problem_->evaluateConstraints(newSolution);
-		evaluations++;
-		population->add(newSolution);
-	} //for
+//  cout << "gGA: Poblacion inicializada con size = " << population->size() << endl;
+//  cout << "gGA: Maximo de evaluaciones: " << maxEvaluations << endl;
 
-	cout << "gGA: Poblacion inicializada con size = " << population->size() << endl;
-	cout << "gGA: Maximo de evaluaciones: " << maxEvaluations << endl;
+  // Generations
+  while (evaluations < maxEvaluations) {
+//    cout << "Evals: " << evaluations << ". Fitness: "
+//        << population->best(comparator)->getObjective(0)<< endl;
 
-	// Generations
-	while (evaluations < maxEvaluations) {
-		cout << "Evals: " << evaluations << ". Fitness: "
-				<< population->best(comparator)->getObjective(0)<< endl;
+    // Create the offSpring solutionSet
+    offspringPopulation = new SolutionSet(populationSize);
+    Solution ** parents = new Solution*[2];
 
-		// Create the offSpring solutionSet
-		offspringPopulation = new SolutionSet(populationSize);
-		Solution ** parents = new Solution*[2];
+    for (int i = 0; i < (populationSize / 2); i++) {
+      if (evaluations < maxEvaluations) {
+        //obtain parents
+        parents[0] = (Solution *) (selectionOperator->execute(population));
+        parents[1] = (Solution *) (selectionOperator->execute(population));
+        Solution ** offSpring = (Solution **) (crossoverOperator->execute(parents));
+        mutationOperator->execute(offSpring[0]);
+        mutationOperator->execute(offSpring[1]);
+        problem_->evaluate(offSpring[0]);
+        problem_->evaluateConstraints(offSpring[0]);
+        problem_->evaluate(offSpring[1]);
+        problem_->evaluateConstraints(offSpring[1]);
 
-		for (int i = 0; i < (populationSize / 2); i++) {
-			if (evaluations < maxEvaluations) {
-				//obtain parents
-				parents[0] = (Solution *) (selectionOperator->execute(population));
-				parents[1] = (Solution *) (selectionOperator->execute(population));
-				Solution ** offSpring = (Solution **) (crossoverOperator->execute(parents));
-				mutationOperator->execute(offSpring[0]);
-				mutationOperator->execute(offSpring[1]);
-				problem_->evaluate(offSpring[0]);
-				problem_->evaluateConstraints(offSpring[0]);
-				problem_->evaluate(offSpring[1]);
-				problem_->evaluateConstraints(offSpring[1]);
+        offspringPopulation->add(offSpring[0]);
+        offspringPopulation->add(offSpring[1]);
+        evaluations += 2;
+        delete[] offSpring;
+      } // if
+    } // for
+    delete[] parents;
 
-				offspringPopulation->add(offSpring[0]);
-				offspringPopulation->add(offSpring[1]);
-				evaluations += 2;
-			} // if
-		} // for
-		/*
-		for (int i = 0; i < (populationSize / 2); i++) {
-			if (evaluations < maxEvaluations) {
-				//obtain parents
-				parents[0] = (Solution *) (selectionOperator->execute(population));
-				parents[1] = (Solution *) (selectionOperator->execute(population));
-				Solution ** offSpring = (Solution **) (crossoverOperator->execute(parents));
-				cout << "CROSSOVER" << endl ;
+    /*
+    for (int i = 0; i < (populationSize / 2); i++) {
+      if (evaluations < maxEvaluations) {
+        //obtain parents
+        parents[0] = (Solution *) (selectionOperator->execute(population));
+        parents[1] = (Solution *) (selectionOperator->execute(population));
+        Solution ** offSpring = (Solution **) (crossoverOperator->execute(parents));
+        cout << "CROSSOVER" << endl ;
 
-				cout << "P0: " << ((Binary *)(parents[0]->getDecisionVariables()[0]))->toString()
-						<< " " << parents[0]->getObjective(0) << endl ;
-				cout << "P1: " << ((Binary *)(parents[1]->getDecisionVariables()[0]))->toString()
-						<< " " << parents[1]->getObjective(0) << endl ;
+        cout << "P0: " << ((Binary *)(parents[0]->getDecisionVariables()[0]))->toString()
+            << " " << parents[0]->getObjective(0) << endl ;
+        cout << "P1: " << ((Binary *)(parents[1]->getDecisionVariables()[0]))->toString()
+            << " " << parents[1]->getObjective(0) << endl ;
 
-				cout << "Of0: " << ((Binary *)(offSpring[0]->getDecisionVariables()[0]))->toString()
-						<< " " << offSpring[0]->getObjective(0) << endl ;
-				cout << "Of1: " << ((Binary *)(offSpring[1]->getDecisionVariables()[0]))->toString()
-						<< " " << offSpring[1]->getObjective(0) << endl ;
+        cout << "Of0: " << ((Binary *)(offSpring[0]->getDecisionVariables()[0]))->toString()
+            << " " << offSpring[0]->getObjective(0) << endl ;
+        cout << "Of1: " << ((Binary *)(offSpring[1]->getDecisionVariables()[0]))->toString()
+            << " " << offSpring[1]->getObjective(0) << endl ;
 
-				mutationOperator->execute(offSpring[0]);
-				mutationOperator->execute(offSpring[1]);
-				cout << "MUTATION" << endl ;
-				problem_->evaluate(offSpring[0]);
-				problem_->evaluateConstraints(offSpring[0]);
-				problem_->evaluate(offSpring[1]);
-				problem_->evaluateConstraints(offSpring[1]);
+        mutationOperator->execute(offSpring[0]);
+        mutationOperator->execute(offSpring[1]);
+        cout << "MUTATION" << endl ;
+        problem_->evaluate(offSpring[0]);
+        problem_->evaluateConstraints(offSpring[0]);
+        problem_->evaluate(offSpring[1]);
+        problem_->evaluateConstraints(offSpring[1]);
 
-				cout << "P0 : " << ((Binary *)(parents[0]->getDecisionVariables()[0]))->toString()
-						<< " " << parents[0]->getObjective(0) << endl ;
-				cout << "P1 : " << ((Binary *)(parents[1]->getDecisionVariables()[0]))->toString()
-						<< " " << parents[1]->getObjective(0) << endl ;
-				cout << "Of0: " << ((Binary *)(offSpring[0]->getDecisionVariables()[0]))->toString()
-						<< " " << offSpring[0]->getObjective(0) << endl ;
-				cout << "Of1: " << ((Binary *)(offSpring[1]->getDecisionVariables()[0]))->toString()
-						<< " " << offSpring[1]->getObjective(0) << endl ;
+        cout << "P0 : " << ((Binary *)(parents[0]->getDecisionVariables()[0]))->toString()
+            << " " << parents[0]->getObjective(0) << endl ;
+        cout << "P1 : " << ((Binary *)(parents[1]->getDecisionVariables()[0]))->toString()
+            << " " << parents[1]->getObjective(0) << endl ;
+        cout << "Of0: " << ((Binary *)(offSpring[0]->getDecisionVariables()[0]))->toString()
+            << " " << offSpring[0]->getObjective(0) << endl ;
+        cout << "Of1: " << ((Binary *)(offSpring[1]->getDecisionVariables()[0]))->toString()
+            << " " << offSpring[1]->getObjective(0) << endl ;
 //exit(0) ;
-				offspringPopulation->add(offSpring[0]);
-				offspringPopulation->add(offSpring[1]);
-				evaluations += 2;
+        offspringPopulation->add(offSpring[0]);
+        offspringPopulation->add(offSpring[1]);
+        evaluations += 2;
 
-				//exit(0) ;
+        //exit(0) ;
 
-			} // if
-		} // for
+      } // if
+    } // for
     */
-		population->sort(comparator) ;
-		offspringPopulation->sort(comparator) ;
+    population->sort(comparator) ;
+    offspringPopulation->sort(comparator) ;
 
-		offspringPopulation->replace(offspringPopulation->size()-1, new Solution(population->get(0))) ;
-		offspringPopulation->replace(offspringPopulation->size()-2, new Solution(population->get(1))) ;
+    delete offspringPopulation->get(offspringPopulation->size()-1);
+    delete offspringPopulation->get(offspringPopulation->size()-2);
+    offspringPopulation->replace(offspringPopulation->size()-1, new Solution(population->get(0))) ;
+    offspringPopulation->replace(offspringPopulation->size()-2, new Solution(population->get(1))) ;
 
-		population->clear() ;
-		for (int i = 0; i < populationSize; i++)
-			population->add(offspringPopulation->get(i)) ;
-		offspringPopulation->clear() ;
-	}
+    for (int i=0;i<population->size();i++) {
+      delete population->get(i);
+    }
+    population->clear() ;
+
+    for (int i = 0; i < populationSize; i++)
+      population->add(offspringPopulation->get(i)) ;
+    offspringPopulation->clear() ;
+    delete offspringPopulation;
+  }
+
+  delete comparator;
 
   SolutionSet * resultPopulation  = new SolutionSet(1) ;
-  resultPopulation->add(population->get(0)) ;
+  resultPopulation->add(new Solution(population->get(0))) ;
+  delete population;
 
   return resultPopulation ;
 } // execute
