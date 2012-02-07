576df8375759196821a68262c88a07bacc9f539d
estebanlc <esteban@lcc.uma.es>
2012-02-07 19:00:55 +0100

    Added GNU license comment to metaheuristic files

--- jmetalcpp/src/metaheuristics/gde3/GDE3.h
+++ jmetalcpp/src/metaheuristics/gde3/GDE3.h
@@ -1,8 +1,22 @@
-/*
- * @file GDE3.h
- * @author Esteban López
- * @date 16 January 2012
- */
+//  GDE3.h
+//
+//  Author:
+//       Esteban López <esteban@lcc.uma.es>
+//
+//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef __GDE3__
 #define __GDE3__
@@ -28,6 +42,5 @@ public:
   GDE3(Problem * problem);
   SolutionSet * execute();
 };
-
 
 #endif /* __GDE3__ */
--- jmetalcpp/src/metaheuristics/nsgaII/NSGAII.h
+++ jmetalcpp/src/metaheuristics/nsgaII/NSGAII.h
@@ -1,8 +1,22 @@
-/*
- * @file NSGAII.h
- * @author Esteban López
- * @date 24 November 2011
- */
+//  NSGAII.h
+//
+//  Author:
+//       Esteban López <esteban@lcc.uma.es>
+//
+//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef __NSGAII__
 #define __NSGAII__
