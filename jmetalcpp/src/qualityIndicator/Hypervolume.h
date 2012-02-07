62432309e340fdabf72c8e5c1989791849c6f2ca
estebanlc <esteban@lcc.uma.es>
2012-02-07 19:01:26 +0100

    Added GNU license comment to quality indicator files

--- jmetalcpp/src/qualityIndicator/Hypervolume.h
+++ jmetalcpp/src/qualityIndicator/Hypervolume.h
@@ -1,12 +1,25 @@
-/*
-  * @file Hypervolume.h
-  * @author Esteban López
-  * @date 13 December 2011
- */
+//  Hypervolume.h
+//
+//  Author:
+//       Esteban López <esteban@lcc.uma.es>
+//
+//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef __HYPERVOLUME__
 #define __HYPERVOLUME__
-
 
 /**
  * This class implements the hypervolume indicator. The code is a C++ version
