e0c3c5619eb23efa0dcf6130616c9156648bb392
Emad Alharbi <emra500@york.ac.uk>
2019-02-12 11:00:45 +0000

    Inversion mutation operator

---
+++ src/operators/mutation/InversionMutation.h
@@ -0,0 +1,30 @@
+/*
+ * InversionMutation.h
+ *
+ *  Created on: 9 Feb 2019
+ *      Author: Emad Alharbi
+ */
+
+#ifndef SRC_OPERATORS_MUTATION_INVERSIONMUTATION_H_
+#define SRC_OPERATORS_MUTATION_INVERSIONMUTATION_H_
+#include <Mutation.h>
+#include <Solution.h>
+#include <math.h>
+#include <float.h>
+#include <PseudoRandom.h>
+#include <Int.h>
+class InversionMutation : public Mutation{
+public:
+	InversionMutation();
+	virtual ~InversionMutation();
+
+	InversionMutation(map<string, void *> parameters);
+
+	  void * execute(void *);
+private:
+  double mutationProbability_;
+  // TODO: VALID_TYPES;
+  void * doMutation(double probability, Solution * solution);
+};
+
+#endif /* SRC_OPERATORS_MUTATION_INVERSIONMUTATION_H_ */
